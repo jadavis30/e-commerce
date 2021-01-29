b3d3d8ae6654025579b79465e4739bf01f47510a
Jared Davis <jared.davis21@yahoo.com>
2021-01-29 18:17:13 -0500

    skeleton tables for db

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,28 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+--CREATE TABLE Category
+CREATE TABLE category(
+    id
+    category_name
+);
+
+CREATE TABLE product(
+    id
+    product_name
+    price
+    stock
+    category_id
+);
+
+CREATE TABLE tag(
+    id
+    tag_name
+);
+
+CREATE TABLE producttag(
+    id
+    product_id
+    tag_id
+);
+
