f6e8c30cdd955b01fa1e5071f17bea1fd8c25557
Jared Davis <jared.davis21@yahoo.com>
2021-01-29 18:31:07 -0500

    updated first product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,6 +6,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  db.Product.findAll({}).then(dbProduct => {
+    res.json(dbProduct);
+  });
   // be sure to include its associated Category and Tag data
 });
 
