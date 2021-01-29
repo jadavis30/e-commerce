08f2012194f00906208786ab3fca1d08dad5a4a4
Jared Davis <jared.davis21@yahoo.com>
2021-01-29 18:28:11 -0500

    db column info added

--- db/schema.sql
+++ db/schema.sql
@@ -6,26 +6,30 @@ CREATE DATABASE ecommerce_db;
 
 --CREATE TABLE Category
 CREATE TABLE category(
-    id
-    category_name
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    category_name "STRING" NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE product(
-    id
-    product_name
-    price
-    stock
-    category_id
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    product_name "STRING" NOT NULL,
+    price DECIMAL NOT NULL --validates that value is a decimal
+    stock INTEGER NOT NULL --default is 10, validates value as numeric
+    category_id INTEGER REFERENCES category_id,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tag(
-    id
-    tag_name
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    tag_name "STRING",
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE producttag(
-    id
-    product_id
-    tag_id
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    product_id INTEGER REFERENCES product_id,
+    tag_id INTEGER REFERENCES tag_id,
+    PRIMARY KEY (id)
 );
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Product, Category } = require('../../models');
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
@@ -6,5 +7,10 @@ const tagRoutes = require('./tag-routes');
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
+
+Product.belongsto(Category);
+Category.hasMany(Product);
+Product.belongstoMany(Tag);
+Tag.belongstoMany(Product);
 
 module.exports = router;
